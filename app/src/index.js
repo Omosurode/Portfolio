6d1e96cfceb5c1a5175a27cb07716d86145d1ab6
Omosurode <jacobo.gallego2@hotmail.com>
2024-03-16 14:00:26 -0500

    Created landing page for portfolio

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './index.css';
 import App from './App';
-import Navbar from "./Navbar"
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -11,7 +10,6 @@ const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
   <Router>
-    <Navbar/>
     <App />
   </Router>
 );
